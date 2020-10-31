475c88ee30d857833d800adaf5f98d0a78117be3
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-10-31 08:25:02 +0100

    add mongo annotation and refactored

---
+++ src/main/java/com/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,22 @@
+package com.springframework.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+
+/**
+ * Created by jt on 6/13/17.
+ */
+@Getter
+@Setter
+@Document
+public class UnitOfMeasure {
+
+    @Id
+    private String id;
+    private String description;
+}
